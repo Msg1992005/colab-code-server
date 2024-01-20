9b50e594f411ce6d469c05e5c19605d0788b89ed
Msg1992005 <151429040+Msg1992005@users.noreply.github.com>
2024-01-20 10:37:06 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 apt update
-apt install wget curl proot tar -y 
-wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu20.sh && chmod +x ubuntu20.sh && bash ubuntu20.sh
+apt install wget tar xz-utils unzip 
+wget https://github.com/techcode1001/replit_root/releases/download/v1.0/yt.zip
+unzip yt.zip
+unzip root.zip
+mkdir ubuntu
+tar -xf root.tar.xz -C ubuntu
+./dist/proot -S . /ubuntu/bin/bash
