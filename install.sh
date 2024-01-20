06455d0ddba7ff838faf7ea0a32fb53ddee6bc5f
Msg1992005 <151429040+Msg1992005@users.noreply.github.com>
2024-01-20 10:26:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 apt-get update
 apt-get install apt-utils
 apt install -y sudo curl nano npm
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+ngrok config add-authtoken 2YIuT9FzD7HiiNChcG26SJRoaVM_2eDMorgHgeyDpHWGf49C4
 curl -fsSL https://code-server.dev/install.sh | sh
+ngrok tcp 8080 &
