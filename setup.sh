36519502a4e9ae6c1a1cc0cc99a4b427674109da
Msg1992005 <151429040+Msg1992005@users.noreply.github.com>
2024-01-20 10:30:08 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+apt update
+apt install wget curl proot tar -y 
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Ubuntu20/ubuntu20.sh -O ubuntu20.sh && chmod +x ubuntu20.sh && bash ubuntu20.sh
